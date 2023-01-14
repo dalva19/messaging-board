504e576be8b8334757e972d8e63e1a55cfdfeae6
dalva19 <alvarado.danna@gmail.com>
2023-01-14 13:12:03 -0500

    packages, layout, styling, and nav

---
+++ components/Nav.jsx
@@ -0,0 +1,42 @@
+import Link from "next/link"
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from "../utils/firebase";
+import {FaUserAlt} from 'react-icons/ai'
+import Image from "next/image";
+
+export default function Nav () {
+  const [user, loading] = useAuthState(auth);
+  console.log(user)
+
+  return (
+   <nav className="flex justify-between items-center py-10">
+    <Link href={"/"}>
+      <button className="text-lg font-medium">Creative Minds</button>
+    </Link>
+
+    <ul className="flex items-center gap-10">
+      {user ? (
+        <div className="flex items-center gap-6">
+             <Link href={"/post"} className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+             <button className="font-medium bg-cyan-500 text-white rounded text-sm">Post</button>
+            </Link>
+            <Link href={"/dashboard"}>
+              {/* <Image className="w-12 rounded-full cursor-pointer" src={user.photoURL} alt="" width={} /> */}
+              {user.photoURL ? (
+                 <img className="w-12 rounded-full cursor-pointer" src={user.photoURL} alt="" />
+              ): (
+                <FaUserAlt />
+              )}
+            
+            </Link>
+            </div>
+      ): (
+        <Link href={"/auth/login"} className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+        Join Now
+       </Link>
+      )}
+      
+    </ul>
+   </nav>
+  )
+}
---
+++ pages/dashboard.jsx
@@ -0,0 +1,9 @@
+export default function Dashboard () {
+  return (
+    <div>
+      <h1>Your Posts</h1>
+      <div>posts</div>
+      <button>Sign Out</button>
+    </div>
+  )
+}
